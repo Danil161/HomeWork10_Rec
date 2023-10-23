c76f970807e0fb30a1eeb674f416f0ffd674968c
Danil <danil.bazarov@inbox.ru>
2023-10-23 15:03:26 +0300

    End Examples001,002,003

---
+++ Example002(66)/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке
+
+int GetSum(int start, int end)
+{
+    if (start > end)
+        return 0;
+
+    return start + GetSum(start + 1, end);
+}
+
+int M = 1;
+int N = 15;
+
+int sum = GetSum(M, N);
+
+Console.WriteLine($"Сумма натуральных элементов от {M} до {N} равна {sum}");
---
+++ Example003(68)/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// Пример:
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
+
+// Ввод значений m и n
+Console.Write("Введите значение m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение n: ");
+int n = int.Parse(Console.ReadLine());
+
+// Вычисление функции Аккермана
+int result = Ackermann(m, n);
+
+// Вывод результата
+Console.WriteLine($"A({m}, {n}) = {result}");
